096a388caf8f1bed65a8c0130d2890f5fdafbdbc
underoatjd <underoatjd@gmail.com>
2023-11-24 23:23:20 -0500

    Inicializando repositorio

---
+++ wdqdqdq.R
@@ -0,0 +1,14 @@
+View(Ventas)
+View(Vendedores)
+head(Ventas,3)
+tail(Ventas,3)
+summary(Ventas)
+resumenVentas <- summary(Ventas)
+View(resumenVentas)
+mes <- Ventas$Mes
+View(mes)
+mes2 <- Ventas[,1]
+summary(mes2)
+fila1 <- Ventas[1,]
+View(fila1)
+summary(fila1)
