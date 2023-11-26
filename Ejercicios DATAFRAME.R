6ce7594db79e75a172e2743b4939024727ae1c4c
underoatjd <underoatjd@gmail.com>
2023-11-26 00:47:37 -0500

    actualizo repo a EjerciciosDATAFRAME

--- Ejercicios DATAFRAME.R
+++ Ejercicios DATAFRAME.R
@@ -10,5 +10,15 @@ dataprueba <- Ventas[, c(1, 6, 9)]
 #matrix para llamar campos especificos
 Ventas[c(1, 6, 9), c(1, 6, 9)]
 #ejercicio 4 de dataprueba filtrar meses < julio y que sector == 1
-dataprueba[]
-install.packages("datos")
+ejercicio = Ventas[Ventas$Mes < 7,]
+ejercicio = Ventas[Ventas$Mes < 7 & Ventas$Producto == "Radio" & Ventas$Sector == 1,c(1,2,3,4,8)]
+VentasRadio <- Ventas[Ventas$Producto == "Radio",]
+#ejercicios 5 y 6 columna llamada "Tipo de compra"
+Ventas$"Tipo de compra" = ifelse(Ventas$`Monto venta`>1000,1,0)
+#ejercicio 7 agregar una columna que se llame utilidad que sea monto de ventas menos 15 de impuestos
+Ventas$Utilidad =Ventas$`Monto venta` * 0.85
+
+
+
+
+
